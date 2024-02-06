58bb6da5ff062bfbc9d3a185e21ff58d41e22c76
Rabbia sana <rabbiasana55@gmail.com>
2024-02-06 16:49:44 +0500

    about us

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Navbar from "./components/Navbar.svelte";
   import CarouselVideo from "./components/CarouselVideo.svelte";
+  import AboutUs from "./components/AboutUs.svelte";
 </script>
 
 <main>
   <Navbar />
   <CarouselVideo />
+  <AboutUs />
 </main>
---
+++ src/components/AboutUs.svelte
@@ -0,0 +1,34 @@
+<script>
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "bootstrap";
+</script>
+
+<div class="container bg-gray">
+  <div class="row text-left mt-5">
+    <h2>About Us</h2>
+  </div>
+  <div class="row mt-5">
+    <div class="col-lg-6">
+      <h1>
+        Work with top notch designers and developers to get amazing products.
+      </h1>
+      <p>
+        Flicken is a service-oriented company providing creative and innovative
+        solutions for your business domain. We believe in exceeding your
+        expectations by delivering thoughtfully innovated eye-catching products
+        on your desk. We take a pride in engineering your requirements into
+        robust software using our mobile, web, cloud and e-commerce
+        capabilities.
+      </p>
+    </div>
+    <div class="col-lg-6">
+      <img src="/images/about.png" alt="" class="w-100" />
+    </div>
+  </div>
+</div>
+
+<style>
+  .bg-gray {
+    background-color: #f9f9f9;
+  }
+</style>
